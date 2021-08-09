e6e66818bf62d1a5833bd225e5cabe1bb78c4a79
doitduri <kdle_@naver.com>
2021-08-09 15:19:30 +0900

    Initial Commit

---
+++ VIPER-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VIPER-SwiftUI
+//
+//  Created by 김두리 on 2021/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
